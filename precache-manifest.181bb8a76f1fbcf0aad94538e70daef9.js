0542a1fded9ff13d4a7ca0984de2fceebddc4bb4
Mateo <gomezmateo94@gmail.com>
2019-09-27 17:26:37 -0500

    initial commit

---
+++ precache-manifest.181bb8a76f1fbcf0aad94538e70daef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc564f5d7ab629bbba355ff15a7102a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64416d23b64415319476",
+    "url": "/static/css/main.5983fb22.chunk.css"
+  },
+  {
+    "revision": "31dfd4626590945ccf1a",
+    "url": "/static/js/2.8530ab98.chunk.js"
+  },
+  {
+    "revision": "64416d23b64415319476",
+    "url": "/static/js/main.43bfbb85.chunk.js"
+  },
+  {
+    "revision": "e6a87a96176380a7e400",
+    "url": "/static/js/runtime-main.a6e95348.js"
+  }
+]);
